ae5b93a70eaf85d5978f20aeb3707cbd0170df99
Faizan Parvez <faizanparvez30@gmail.com>
2025-08-28 00:19:18 +0530

    Pattern Questions

---
+++ src/practicePattern4.java
@@ -0,0 +1,11 @@
+public class practicePattern4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j < i+2; j++) {
+                System.out.print(" " + j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
