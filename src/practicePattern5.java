87d509965381e034e599a4741df8ccd3d6f4463d
Faizan Parvez <faizanparvez30@gmail.com>
2025-08-29 00:39:16 +0530

    Complex Pattern Questions

---
+++ src/practicePattern5.java
@@ -0,0 +1,10 @@
+public class practicePattern5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(i+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
